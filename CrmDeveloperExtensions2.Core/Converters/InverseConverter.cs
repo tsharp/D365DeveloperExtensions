6a7c0a238c95e88acf2b779988e350a5e036566d
Jason Lattimer <jason.lattimer@gmail.com>
2017-09-07 08:26:38 -0500

    Styling

---
+++ CrmDeveloperExtensions2.Core/Converters/InverseConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CrmDeveloperExtensions2.Core.Converters
+{
+    public class InverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return !bool.Parse(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebResourceDeployer/Converters/StateConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WebResourceDeployer.Converters
+{
+    public class StateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool state = Boolean.Parse(value.ToString());
+
+            return state ? "Managed" : "Unmanaged";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
