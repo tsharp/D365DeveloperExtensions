c9839fb654295dd241bd14a56ad74ac651365dd1
Jason Lattimer <jason.lattimer@gmail.com>
2017-11-11 22:40:25 -0600

    Updates to templating

--- CrmDeveloperExtensions2.Core/Models/MockingFramework.cs
+++ CrmDeveloperExtensions2.Core/Models/MockingFramework.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 
 namespace CrmDeveloperExtensions2.Core.Models
 {
@@ -11,15 +11,30 @@ namespace CrmDeveloperExtensions2.Core.Models
 
     public static class MockingFrameworks
     {
-        public static List<MockingFramework> Frameworks => new List<MockingFramework>
+        public static ObservableCollection<MockingFramework> GetMockingFrameworks()
         {
-            new MockingFramework{CrmMajorVersion = 8, Name = "XrmUnitTest", NugetName = "XrmUnitTest.2016"},
-            new MockingFramework{CrmMajorVersion = 7, Name = "XrmUnitTest", NugetName = "XrmUnitTest.2015"},
-            new MockingFramework{CrmMajorVersion = 8, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.365"},
-            new MockingFramework{CrmMajorVersion = 8, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.2016"},
-            new MockingFramework{CrmMajorVersion = 7, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.2015"},
-            new MockingFramework{CrmMajorVersion = 6, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.2013"},
-            new MockingFramework{CrmMajorVersion = 5, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy"}
-        };
+            return new ObservableCollection<MockingFramework> {
+                    new MockingFramework{CrmMajorVersion = 8, Name = "XrmUnitTest", NugetName = "XrmUnitTest.2016"},
+                    new MockingFramework{CrmMajorVersion = 7, Name = "XrmUnitTest", NugetName = "XrmUnitTest.2015"},
+                    new MockingFramework{CrmMajorVersion = 9, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.9"},
+                    new MockingFramework{CrmMajorVersion = 8, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.365"},
+                    new MockingFramework{CrmMajorVersion = 8, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.2016"},
+                    new MockingFramework{CrmMajorVersion = 7, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.2015"},
+                    new MockingFramework{CrmMajorVersion = 6, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.2013"},
+                    new MockingFramework{CrmMajorVersion = 5, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy"}
+            };
+        }
+
+        //private  ObservableCollection<MockingFramework> Frameworks => new ObservableCollection<MockingFramework>
+        //{
+        //    new MockingFramework{CrmMajorVersion = 8, Name = "XrmUnitTest", NugetName = "XrmUnitTest.2016"},
+        //    new MockingFramework{CrmMajorVersion = 7, Name = "XrmUnitTest", NugetName = "XrmUnitTest.2015"},
+        //    new MockingFramework{CrmMajorVersion = 9, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.9"},
+        //    new MockingFramework{CrmMajorVersion = 8, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.365"},
+        //    new MockingFramework{CrmMajorVersion = 8, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.2016"},
+        //    new MockingFramework{CrmMajorVersion = 7, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.2015"},
+        //    new MockingFramework{CrmMajorVersion = 6, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy.2013"},
+        //    new MockingFramework{CrmMajorVersion = 5, Name = "FakeXrmEasy", NugetName = "FakeXrmEasy"}
+        //};
     }
 }
---
+++ TemplateWizards/ProjectDataHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TemplateWizards
+{
+    public class ProjectDataHandler
+    {
+        public static void AddOrUpdateReplacements(string key, string value, ref Dictionary<string, string> replacementsDictionary)
+        {
+            if (replacementsDictionary.ContainsKey(key))
+                replacementsDictionary[key] = value;
+            else
+                replacementsDictionary.Add(key, value);
+        }
+    }
+}
