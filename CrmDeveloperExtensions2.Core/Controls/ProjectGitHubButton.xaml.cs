a0df00cfa44e36a9596f023ffab2dc9e3652cf0f
Jason Lattimer <jason.lattimer@gmail.com>
2017-11-24 11:54:55 -0600

    Added project toolbar buttons

---
+++ CrmDeveloperExtensions2.Core/Controls/ProjectGitHubButton.xaml.cs
@@ -0,0 +1,23 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System.Windows;
+
+namespace CrmDeveloperExtensions2.Core.Controls
+{
+    public partial class ProjectGitHubButton
+    {
+        public ProjectGitHubButton()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void OpenGitHub_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(Package.GetGlobalService(typeof(DTE)) is DTE dte))
+                return;
+
+            WebBrowser.OpenUrl(dte, "https://github.com/jlattimer/CrmDeveloperExtensions2");
+        }
+    }
+}
---
+++ CrmDeveloperExtensions2.Core/Controls/SpklGitHubButton.xaml.cs
@@ -0,0 +1,23 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System.Windows;
+
+namespace CrmDeveloperExtensions2.Core.Controls
+{
+    public partial class SpklGitHubButton
+    {
+        public SpklGitHubButton()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void OpenGitHub_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(Package.GetGlobalService(typeof(DTE)) is DTE dte))
+                return;
+
+            WebBrowser.OpenUrl(dte, "https://github.com/scottdurow/SparkleXrm/wiki/spkl");
+        }
+    }
+}
