ab27e6664ff8946fff3313b2f7090635aa5a79b3
Jason Lattimer <jason.lattimer@gmail.com>
2017-12-12 19:46:50 -0600

    Refactoring

--- PluginTraceViewer/Models/FilterMessage.cs
+++ PluginTraceViewer/Models/FilterMessage.cs
@@ -1,37 +1,13 @@
-﻿using CrmDeveloperExtensions2.Core.DataGrid;
+﻿using PluginTraceViewer.Resources;
 using PluginTraceViewer.ViewModels;
 using System;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace PluginTraceViewer.Models
 {
-    public class FilterMessage : INotifyPropertyChanged, IFilterProperty
+    public class FilterMessage : FilterBase
     {
-        private bool _isSelected;
-
-        public string Name { get; set; }
-        public string Value { get; set; }
-
-        public bool IsSelected
-        {
-            get => _isSelected;
-            set
-            {
-                _isSelected = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
         public static ObservableCollection<FilterMessage> CreateFilterList(ObservableCollection<CrmPluginTrace> traces)
         {
             ObservableCollection<FilterMessage> filterMessages = new ObservableCollection<FilterMessage>(traces.GroupBy(t => t.MessageName).Select(x =>
@@ -46,7 +22,7 @@ namespace PluginTraceViewer.Models
 
             filterMessages.Insert(0, new FilterMessage
             {
-                Name = "Select All",
+                Name = Resource.FilterEntity_Select_All,
                 Value = String.Empty,
                 IsSelected = true
             });
