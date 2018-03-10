1b0b478c7214c43a87fedc20215533dd60d1e1f3
Jason Lattimer <jason.lattimer@gmail.com>
2018-03-10 17:21:41 -0600

    Relaxed VS 2017 dependecies & prerequisites

--- D365DeveloperExtensions/Properties/AssemblyInfo.cs
+++ D365DeveloperExtensions/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.1869.2013")]
-[assembly: AssemblyFileVersion("2.0.1869.2013")]
+[assembly: AssemblyVersion("2.0.1869.2314")]
+[assembly: AssemblyFileVersion("2.0.1869.2314")]
