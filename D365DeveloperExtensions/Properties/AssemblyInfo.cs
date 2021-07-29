61e22ac4fec585361d43c2273bfc7f879aaa2ef2
Bilal Itani <bitani@redcross.ca>
2021-07-29 16:07:04 -0400

    Removed app.config files.

--- D365DeveloperExtensions/Properties/AssemblyInfo.cs
+++ D365DeveloperExtensions/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.21210.1948")]
-[assembly: AssemblyFileVersion("2.0.21210.1948")]
+[assembly: AssemblyVersion("2.0.21210.2006")]
+[assembly: AssemblyFileVersion("2.0.21210.2006")]
