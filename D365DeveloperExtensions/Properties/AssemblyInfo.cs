9f7c4a23d1953a2a24f764d696317b8ba42ea6b0
Travis Sharp <2798069+tsharp@users.noreply.github.com>
2022-03-17 00:33:35 -0700

    Changing SDK versions to 17.0.x

--- D365DeveloperExtensions/Properties/AssemblyInfo.cs
+++ D365DeveloperExtensions/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.2276.0712")]
-[assembly: AssemblyFileVersion("2.0.2276.0712")]
+[assembly: AssemblyVersion("2.0.2276.0730")]
+[assembly: AssemblyFileVersion("2.0.2276.0730")]
